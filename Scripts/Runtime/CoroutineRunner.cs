ef3bc00570914736d141f9e3a76ac057438b80f2
evgeny.vasilyev <evgeny.vasilyev@i3-corps.com>
2021-11-24 16:18:42 -0600

    Renamed TweenRunner to CoroutineRunner

--- Scripts/Runtime/CoroutineRunner.cs
+++ Scripts/Runtime/CoroutineRunner.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace EnvDev
 {
-    public class TweenRunner
+    public class CoroutineRunner
     {
         public event Action Started;
         public event Action Stopped;
@@ -14,16 +14,16 @@ namespace EnvDev
         public event Action Interrupted;
 
         /// <summary>
-        /// True if the runner has any tweens playing
+        /// True if the runner has any coroutines that are running
         /// </summary>
         public bool IsRunning { get; private set; }
 
         readonly MonoBehaviour m_Target;
-        readonly List<TweenRunner> m_Runners = new List<TweenRunner>();
+        readonly List<CoroutineRunner> m_Runners = new List<CoroutineRunner>();
 
         Coroutine m_Coroutine;
 
-        public TweenRunner(MonoBehaviour target)
+        public CoroutineRunner(MonoBehaviour target)
         {
             m_Target = target;
         }
@@ -31,32 +31,32 @@ namespace EnvDev
         public void Run(IEnumerator tweenRoutine)
         {
             Interrupt();
-            m_Coroutine = m_Target.StartCoroutine(RunRoutine(tweenRoutine));
+            m_Coroutine = m_Target.StartCoroutine(WaitFor(tweenRoutine));
         }
 
-        public void Run(params IEnumerator[] tweenRoutines)
+        public void Run(params IEnumerator[] coroutines)
         {
-            if (tweenRoutines.Length == 1)
+            var coroutineCount = coroutines.Length;
+
+            if (coroutineCount == 1)
             {
-                Run(tweenRoutines[0]);
+                Run(coroutines[0]);
                 return;
             }
 
             Interrupt();
 
-            var tweenCount = tweenRoutines.Length;
-            
-            while (tweenCount > m_Runners.Count)
-                m_Runners.Add(new TweenRunner(m_Target));
+            while (coroutineCount > m_Runners.Count)
+                m_Runners.Add(new CoroutineRunner(m_Target));
 
-            for (var i = 0; i < tweenCount; i++)
-                m_Runners[i].Run(tweenRoutines[i]);
+            for (var i = 0; i < coroutineCount; i++)
+                m_Runners[i].Run(coroutines[i]);
 
-            m_Coroutine = m_Target.StartCoroutine(WaitForAll(m_Runners, tweenCount));
+            m_Coroutine = m_Target.StartCoroutine(WaitForAll(m_Runners, coroutineCount));
         }
 
         /// <summary>
-        /// Stops all tweens
+        /// Stops execution of all coroutines
         /// </summary>
         public void Interrupt()
         {
@@ -74,34 +74,34 @@ namespace EnvDev
             Stopped?.Invoke();
         }
 
-        IEnumerator RunRoutine(IEnumerator routine)
+        IEnumerator WaitFor(IEnumerator coroutine)
         {
             IsRunning = true;
             Started?.Invoke();
 
-            yield return routine;
+            yield return coroutine;
 
             IsRunning = false;
             Completed?.Invoke();
             Stopped?.Invoke();
         }
 
-        IEnumerator WaitForAll(List<TweenRunner> runners, int tweenCount)
+        IEnumerator WaitForAll(List<CoroutineRunner> runners, int coroutineCount)
         {
             IsRunning = true;
             Started?.Invoke();
 
-            var runningTweenIndex = 0;
+            var runningCoroutineIndex = 0;
             while (true)
             {
-                if (runners[runningTweenIndex].IsRunning)
+                if (runners[runningCoroutineIndex].IsRunning)
                 {
                     yield return null;
                 }
                 else
                 {
-                    runningTweenIndex++;
-                    if (runningTweenIndex >= tweenCount)
+                    runningCoroutineIndex++;
+                    if (runningCoroutineIndex >= coroutineCount)
                         break;
                 }
             }
